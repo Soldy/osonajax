57f5186721870f928fd451469a19c301d612bb1d
testa <testa.a.tapos@gmail.com>
2013-07-11 11:46:28 +0100

    Formating

--- ajax.js
+++ ajax.js
@@ -1,22 +1,12 @@
-                function osonnetsend(command, fnam) {
+                function osonajaxnetsend(command, fnam) {
                         var xhReq = new XMLHttpRequest();
                         xhReq.open("POST", "/", false);
                         xhReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                         xhReq.onreadystatechange  = function(){
                             if (xhReq.readyState == 4){
-                                var osonajaObj = JSON.parse(xhReq.responseText);
-                                singer.playlistnew = 0 ;                             
+                                var osonajaObj = JSON.parse(xhReq.responseText);                            
                                 for (i=0;osonajaObj.length>i;i++) {
-                                     playerinput(jsonObj[i]);
-                                }
-                                osonajaObj.name;
-                                if (singer.playlistnew >0){
-                                    playerlistmake();
-                                    playerinterfacefl(singer.playlistbyid[singer.singi].lengt);
-                                    playerinterbarpos(singer.playlistbyid[singer.singi].lengt);
-                                    if (singer.singed.toString() === "none"){
-                                        singerpush(singer.singi)
-                                    }
+                                     osonajaxcomcent(jsonajaObj[i]);
                                 }
                             }
                         }
